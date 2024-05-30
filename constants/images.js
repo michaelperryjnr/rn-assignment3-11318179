894b9ca78087643dd0ce6327cd7e9945db2e66c2
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-30 11:24:18 +0000

    Image exports

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/profile.png";
+import learning from "../assets/images/learning.png";
+import exercise from "../assets/images/exercise.png";
+
+export default { profile, learning, exercise };
