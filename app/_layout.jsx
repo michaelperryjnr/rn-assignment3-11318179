72f7bbe9ae0711efcaa3b8288886d487fb3d9be2
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-30 11:27:34 +0000

    Created default app layout

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { SplashScreen, Stack } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
